b3ce12db8a12bef9703eef77a47a8cd1b527642a
Paul Timke <87957114+paultimke@users.noreply.github.com>
2023-01-06 16:10:53 -0600

    Add files I untracked by accident

---
+++ Emulator/src/registers.rs
@@ -0,0 +1,23 @@
+// Constants
+pub const REG_TOTAL_NUM: usize = 8; // Total num of General Purpose Registers
+pub const MBR_PTR: usize = 7;       // Address of General Purpose Register MBR
+pub const SP_PTR: usize = 5;        // Address of General Purpose Register SP
+
+// Register type, gerneral purpose and special purpose
+pub struct Registers {
+    pub gp: [i16; REG_TOTAL_NUM],
+    pub pc:  u16,
+    pub acc: i16,
+    pub ir:  u16,
+    pub mar: u16,
+} 
+
+impl Registers {
+    pub fn new() -> Self {
+        Self {
+            gp: [0; REG_TOTAL_NUM],
+            pc: 0, acc: 0,
+            ir: 0, mar: 0
+        }
+    }
+}
